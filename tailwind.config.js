b82d76336ccc66ed3eb24a6837056a238048a7ec
Jithendarramagiri <138892392+Jithendarramagiri1998@users.noreply.github.com>
2025-08-01 01:55:21 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: "class", // ✅ Add this line for dark mode support
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -8,4 +9,4 @@ export default {
     extend: {},
   },
   plugins: [],
-}
+};
